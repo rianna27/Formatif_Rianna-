3604bc5811a4078ac14ceb9230dce591b769c459
riannasmuk <riannasmuk@10.5.2.116>
2020-12-10 22:28:39 -0500

    Ne regarde pas à la correction de l'exercise 1. Celui là je pense est la meilleur.

--- Formatif/src/Formatif_Rianna.java
+++ Formatif/src/Formatif_Rianna.java
@@ -2,13 +2,7 @@
 *Author : riannasmuk10-Dec-2020
 *Date : 10-Dec-2020
 */
-public class Formatif_Rianna
-
-{
-		        /*
-				 * Bonjour. Écrire une méthode permettant de calculer et d’afficher la somme des éléments
-				 * d’un tableau d’entiers. 
-				 */
+public class Formatif_Rianna {
 
 		public static void main(String[] args) {
 				int[] myTab = {23, 33, 78, 98, 12, 76, 67};
